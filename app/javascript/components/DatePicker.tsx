8ac1c3abe38fe8ba4e9e0335ac30c531292b62a8
takayama shunya <63427166+takayama-shunya@users.noreply.github.com>
2023-08-28 20:39:21 +0900

    カレンダーを日本語表示に変更

---
+++ app/javascript/components/DatePicker.tsx
@@ -0,0 +1,31 @@
+import ja from 'date-fns/locale/ja'
+import dayjs from 'dayjs'
+import React from 'react'
+import ReactDatePicker, { ReactDatePickerProps, registerLocale } from 'react-datepicker'
+
+registerLocale('ja', ja)
+
+export const DatePicker = (props: ReactDatePickerProps) => {
+  return (
+    <div className="light-theme-original">
+      <ReactDatePicker
+        locale={ja}
+        renderCustomHeader={({ date, decreaseMonth, increaseMonth }) => (
+          <div className="datepicker__header">
+            <button className="datepicker__button" onClick={decreaseMonth}>
+              {'<'}
+            </button>
+            <div className="datepicker__header-date">
+              <div className="datepicker__header-date__year">{dayjs(date).year()}年</div>
+              <div className="datepicker__header-date__month">{dayjs(date).month() + 1}月</div>
+            </div>
+            <button className="datepicker__button" onClick={increaseMonth}>
+              {'>'}
+            </button>
+          </div>
+        )}
+        {...props}
+      />
+    </div>
+  )
+}
--- app/javascript/components/ScoutNewForm.tsx
+++ app/javascript/components/ScoutNewForm.tsx
@@ -1,23 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { DatePicker } from "../components/DatePicker";
 import { Box } from "@chakra-ui/react";
-import { useState } from "react";
-import DatePicker from "react-datepicker";
-
-// import "/stylesheets/datepicker/react-datepicker.css";
-// import "app/assets/stylesheets/datepicker/test.scss"
 
 export default function Home() {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
-    <div>
-      <p className="test-p">test</p>
-      <Box w="250px" mt="30px" ml="30px">
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-        />
-      </Box>
-    </div>
+    <Box w="250px" mt="30px" ml="30px">
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="yyyy/MM/dd"
+        calendarStartDay={1}
+      />
+    </Box>
   );
 }
