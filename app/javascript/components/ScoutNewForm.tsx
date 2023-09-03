05341a3e9be5864fc4172277141337cadbaad43c
takayama shunya <63427166+takayama-shunya@users.noreply.github.com>
2023-09-03 22:03:40 +0900

    formのUIをMUIに統一　バリデーション修正

--- app/javascript/components/ScoutNewForm.tsx
+++ app/javascript/components/ScoutNewForm.tsx
@@ -1,18 +1,90 @@
 import React, { useState } from "react";
-import { DatePicker } from "../components/DatePicker";
-import { Box } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"; 
+import { ScoutNewFormSchema, ScoutNewFormSchemaValidate } from "../components/validate/ScoutNewFormSchema";
+import {
+  Button, Checkbox, Container, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, InputLabel, MenuItem, NativeSelect, Radio, RadioGroup, Select, Stack, TextField
+} from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function Home() {
-  const [startDate, setStartDate] = useState(new Date());
+type formType = {
+  date: Date,
+  place: number,
+}
+
+
+
+
+export default function ScoutNewForm() {
+  
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs(new Date()));
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ScoutNewFormSchemaValidate>({
+    resolver: zodResolver(ScoutNewFormSchema),
+  });
+
+  const onSubmit = (data: ScoutNewFormSchemaValidate) => {
+    console.log(data);
+  };
+
+  // const { register, handleSubmit } = useForm<formType>()
+
+  // const onSubmit = (data: formType) => {
+  //   console.log(data)
+  // }
 
   return (
-    <Box w="250px" mt="30px" ml="30px">
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        dateFormat="yyyy/MM/dd"
-        calendarStartDay={1}
-      />
-    </Box>
+    <Container maxWidth="sm">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+
+          <FormControl>
+            <InputLabel variant="standard" htmlFor="date"></InputLabel>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>
+                <DatePicker
+                  label="日付"
+                  {...register("date")}
+                  value={value}
+                  onChange={(newValue) => setValue(newValue)}
+                />
+              </DemoContainer>
+              <FormHelperText>
+              {errors.date && errors.date.message}
+            </FormHelperText>
+            </LocalizationProvider>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel variant="standard" htmlFor="place">
+              場所
+            </InputLabel>
+            <NativeSelect
+              defaultValue={0}
+              {...register("place")}
+            >
+              <option value={0}>選択してください</option>
+              <option value={1}>東家</option>
+              <option value={2}>南家</option>
+              <option value={3}>西家</option>
+              <option value={4}>北家</option>
+            </NativeSelect>
+            <FormHelperText>
+              {errors.place && errors.place.message}
+            </FormHelperText>
+          </FormControl>
+
+          <Button type="submit" color="primary" variant="contained">
+            送信
+          </Button>
+
+
+        </Stack>
+      </form>
+    </Container>
   );
 }
---
+++ app/javascript/components/validate/ScoutNewFormSchema.tsx
@@ -0,0 +1,21 @@
+import {z} from 'zod';
+
+export const ScoutNewFormSchema = z.object({
+  // 日付に変換してからバリデーションをかける
+  date: z.coerce.date().refine(
+    (val) => val !== null, { message: '日付を選択してください' }
+  ),
+ 
+   
+  // 数値に変換してからバリデーションをかける
+  place: z.preprocess(
+    (val) => Number(val),
+    z
+      .number()
+      .min(1, { message: '場所を選択してください' })
+      .max(4, { message: '値が不正です' }),
+  )
+
+});
+
+export type ScoutNewFormSchemaValidate = z.infer<typeof ScoutNewFormSchema>;
